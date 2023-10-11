d2a729574da01c73cfd2ee2c000c11dcf348fea1
Miranda CONG <minsheu.cong@gmail.com>
2023-10-12 00:17:35 +0800

    wip

---
+++ README.md
@@ -0,0 +1,9 @@
+README FILE
+
+# Hello world
+
+* Bulletpoint
+
+This is math in README file:
+
+$$ \sqrt{\mathbf{x}}$$
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-//! Entry point for the library
-//! 
+// Entry point for the zkMatrix library
+//
+//! Zero-knowledge proof for linear algebra 
+//!
+#![doc = include_str!("../README.md")]
+
 extern crate sprs;
 
 pub mod utils;
