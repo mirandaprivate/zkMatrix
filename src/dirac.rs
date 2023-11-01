d46bcfa30e8ee5f9c4810e8aeff7c3f92dbd8515
beta <jiangyouxin@jiangyouxindeMacBook-Pro.local>
2023-11-01 20:37:22 +0800

    箔

--- src/dirac.rs
+++ src/dirac.rs
@@ -1,23 +1,10 @@
-use bls12_381::{Scalar, G1Projective, G2Projective, pairing, Gt, G1Affine, G2Affine};
+use bls12_381::{pairing, G1Affine, G1Projective, G2Affine, G2Projective, Gt, Scalar};
 use rand::Rng;
-use std::thread;
 use std::sync::Arc;
+use std::thread;
 
 fn random_scalar<R: Rng + ?Sized>(rng: &mut R) -> Scalar {
-    Scalar::from_raw([
-        rng.gen::<u64>(),
-        rng.gen::<u64>(),
-        rng.gen::<u64>(),
-        rng.gen::<u64>()
-    ])
-}
-
-fn random_g1<R: Rng + ?Sized>(rng: &mut R) -> G1Projective {
-    G1Projective::generator() * random_scalar(rng)
-}
-
-fn random_g2<R: Rng + ?Sized>(rng: &mut R) -> G2Projective {
-    G2Projective::generator() * random_scalar(rng)
+    Scalar::from_raw([rng.gen(), rng.gen(), rng.gen(), rng.gen()])
 }
 
 const MATRIX_SIZE: usize = 64;
@@ -26,49 +13,30 @@ const NUM_THREADS: usize = 8;
 
 fn dirac() {
     let mut rng = rand::thread_rng();
-    let mut m: Vec<(usize, usize, Scalar)> = Vec::new();
+    let m = Arc::new((0..NUM_NONZERO_ENTRIES).map(|_| 
+        (rng.gen_range(0..MATRIX_SIZE), rng.gen_range(0..MATRIX_SIZE), random_scalar(&mut rng))
+    ).collect::<Vec<_>>());
+    
+    let v1 = Arc::new((0..MATRIX_SIZE).map(|_| G1Projective::generator() * random_scalar(&mut rng)).collect::<Vec<_>>());
+    let aggregate_intermediate = m.chunks(NUM_NONZERO_ENTRIES / NUM_THREADS).map(|chunk| {
+        let v1 = Arc::clone(&v1);
+        let chunk = chunk.to_owned();
+        thread::spawn(move || chunk.iter().fold(vec![G1Projective::identity(); MATRIX_SIZE], |mut acc, &(row, col, ref val)| {
+            acc[row] += v1[col] * val;
+            acc
+        }))
+    }).fold(vec![G1Projective::identity(); MATRIX_SIZE], |acc, handle| 
+        handle.join().unwrap().iter().enumerate().map(|(i, &l)| l + acc[i]).collect()
+    );
 
-    for _ in 0..NUM_NONZERO_ENTRIES {
-        let i = rng.gen_range(0..MATRIX_SIZE);
-        let j = rng.gen_range(0..MATRIX_SIZE);
-        m.push((i, j, random_scalar(&mut rng)));
-    }
-
-    let v1: Vec<G1Projective> = (0..MATRIX_SIZE).map(|_| random_g1(&mut rng)).collect();
-    let v2: Vec<G2Projective> = (0..MATRIX_SIZE).map(|_| random_g2(&mut rng)).collect();
-
-    let v1_arc = Arc::new(v1);
-
-    let chunk_size = NUM_NONZERO_ENTRIES / NUM_THREADS;
-    let m_chunks: Vec<Vec<(usize, usize, Scalar)>> = m.chunks(chunk_size).map(|chunk| chunk.to_vec()).collect();
-
-    let mut aggregate_intermediate = vec![G1Projective::identity(); MATRIX_SIZE];
-    let mut handles: Vec<_> = m_chunks.into_iter().map(|chunk| {
-        // let v1_clone = v1.clone();
-        let v1_clone = v1_arc.clone();
-        thread::spawn(move || {
-            let mut local_intermediate = vec![G1Projective::identity(); MATRIX_SIZE];
-            for &(row, col, ref val) in &chunk {
-                local_intermediate[row] += v1_clone[col] * val;
-            }
-            local_intermediate
-        })
-    }).collect();
-
-    for handle in handles.drain(..) {
-        let local_intermediate = handle.join().unwrap();
-        for i in 0..MATRIX_SIZE {
-            aggregate_intermediate[i] += local_intermediate[i];
-        }
-    }
-
-    let mut result = Gt::identity();
-    for i in 0..MATRIX_SIZE {
-        result += pairing(&G1Affine::from(aggregate_intermediate[i]), &G2Affine::from(v2[i]));
-    }
+    let v2 = (0..MATRIX_SIZE).map(|_| G2Projective::generator() * random_scalar(&mut rng)).collect::<Vec<_>>();
+    let result = aggregate_intermediate.iter().enumerate().map(|(i, &aggr)| 
+        pairing(&G1Affine::from(aggr), &G2Affine::from(&v2[i]))
+    ).fold(Gt::identity(), |acc, x| acc + x);
 
     println!("Result: {:?}", result);
 }
+
 
 #[cfg(test)]
 mod tests {
